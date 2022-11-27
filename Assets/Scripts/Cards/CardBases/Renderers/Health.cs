1682cc46d0b4845b85527f26d6ab47f08872dec1
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-11-27 15:13:01 -0800

    Shark Card Art
    Added the art for the shark card!

---
+++ Assets/Scripts/Cards/CardBases/Renderers/Health.cs
@@ -0,0 +1,12 @@
+﻿namespace Card.Renderers {
+	public class Health: Base {
+		public TMPro.TMP_Text health;
+
+		public void Update() {
+			base.Update();
+
+			if (card is HealthCardBase hCard)
+				health.text = "" + hCard.health;
+		}
+	}
+}
