27267b072b08f76b5593039c2f522605147cc3e0
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-12-02 17:19:08 -0800

    Added SQLite Library
    https://github.com/praeclarum/sqlite-net
    Database Test!

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using UnityEngine;
+
+public static class DatabaseManager {
+	private static SQLiteConnection _database;
+	public static SQLiteConnection database {
+		get {
+			_database ??= new SQLiteConnection(Application.persistentDataPath + "/castaway.db");
+			return _database;
+		}
+	}
+}
---
+++ Assets/Scripts/SQLDatabaseTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SQLite;
+using UnityEngine;
+
+public class SQLDatabaseTest : MonoBehaviour {
+	public class Stock {
+		[PrimaryKey, AutoIncrement] public int Id { get; set; }
+		public string Symbol { get; set; }
+	}
+
+	public class Valuation {
+		[PrimaryKey, AutoIncrement] public int Id { get; set; }
+		[Indexed] public int StockId { get; set; }
+		public DateTime Time { get; set; }
+		public decimal Price { get; set; }
+	}
+
+	public void Start() {
+		var db = DatabaseManager.database;
+		db.CreateTable<Stock>();
+		db.CreateTable<Valuation>();
+
+		Debug.Log(Application.persistentDataPath + "/castaway.db");
+		
+		var stock = new Stock {
+			Symbol = "ABC"
+		};
+		db.Insert(stock);
+		Debug.Log($"{stock.Symbol} == {stock.Id}");
+
+		var query = db.Table<Stock>().Where(v => v.Symbol.StartsWith("A"));
+		foreach (var s in query)
+			Debug.Log("Stock: " + s.Symbol);
+	}
+}
