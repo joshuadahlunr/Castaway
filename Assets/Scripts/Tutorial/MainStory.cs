3b76ad6eab636bb638a4d2c9f03ea32c2c0c111a
dana <80922081+dananac@users.noreply.github.com>
2023-03-07 16:31:41 -0800

    Add files via upload

---
+++ Assets/Scripts/Tutorial/MainStory.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary> Loads Tutorial Intro </summary>
+/// <author> Dana Conley </author>
+
+public class MainStory : MonoBehaviour
+{
+    void OnEnable()
+    {
+        SceneManager.LoadScene("TutorialSecond", LoadSceneMode.Single);
+    }
+}
