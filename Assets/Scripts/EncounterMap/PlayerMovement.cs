8738c959e1ca4ce6a54f8536491a023a9d697327
white-jared <jaredm0520@gmail.com>
2023-03-28 16:55:23 -0700

    Changes to EncounterMap
    - Map BG is now layered so the map and ship do not overlap the outline the borders of the map and are contained within these boundaries
    - Added Player Input Action
    - Changed PlayerSprite to regular GameObject instead of Prefab
    - Collisions with Encounter Nodes cause the Encounter to trigger and change scene
    - Clicking an Encounter Node gets the position of that node instead of setting the scene
    - Movement WIP cause things keep exploding :sob:

--- Assets/Scripts/EncounterMap/PlayerMovement.cs
+++ Assets/Scripts/EncounterMap/PlayerMovement.cs
@@ -1,79 +1,95 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.EventSystems;
 
 /// <summary>
 /// </summary>
 /// <author>Jared White</author>
 /// </summary>
 namespace EncounterMap {
-    public class PlayerMovement : MonoBehaviour, IPointerClickHandler {
-        private Vector2 startPos;
-        private Vector2 mousePos;
-        private Vector2 newPos;
+    public class PlayerMovement : MonoBehaviour {
+        // Set the target node position from the MapNode script
+        public static Vector2 targetPos;
+        
+        private float speed = 2f;
+        private bool moving;
 
-        void Start() {
-            startPos = transform.position;
+        MapNode[] nodes;
+        // Used to track player progression
+        public int currentNode;
+
+        private Camera mainCamera;
+        private Rigidbody2D playerRB;
+        private BoxCollider2D playerCol;
+
+    /*    void Start() { 
+            // Set the Rigidbody to the player GameObject
+            playerRB = GetComponent<Rigidbody2D>();
+            // Set the BoxColider to the player GameObject
+            playerCol = GetComponent<BoxCollider2D>();
+            // Set the Camera to the main camera
+            mainCamera = Camera.main;
+            // Set the nodes array to get the MapNode components in the parent object (Map)
+            nodes = GetComponentsInParent<MapNode>();
         }
 
-        void Update() {
-            mousePos = Mouse.current.position.ReadValue();
-            if (Mouse.current.leftButton.wasPressedThisFrame){
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(mousePos);
-                if(Physics.Raycast(ray, out hit)){
-                    newPos = hit.point;
-                    transform.position = newPos;
-                }
+        public void Move(InputAction.CallbackContext context) {
+            if ((Vector2)transform.position != (Vector2)targetPos) {
+                transform.position = Vector2.Lerp((Vector2)transform.position, (Vector2)targetPos, Time.deltaTime);
             }
         }
 
-        public void OnPointerClick(PointerEventData eventData){
-            Debug.Log("Clicked");
+        public void CheckNode() {
+            
         }
 
-        /* Figure out how to get tweening or lerp to work in order to follow
-        the time it would take for the player to traverse between destinations.
-        Figure out how to not get the scene switch to be instantaneous
+      public void Move(InputAction.CallbackContext context) {
+            //playerRB.velocity = Vector2.zero;
+            //Vector2 direction = (Vector2)targetPos;
+            //direction.Normalize();
+            if (targetPos != null) {
+                if (coroutine != null) {
+                    StopCoroutine(coroutine);
+                }
+
+                coroutine = StartCoroutine(PlayerMoveTowards((Vector2)targetPos));
+            }
+        }
+
+        public IEnumerator PlayerMoveTowards (Vector2 target) {
+            Vector2 distance = target - playerRB.position;
+            while (distance.sqrMagnitude > float.Epsilon) {
+                Vector2 newPos = Vector2.MoveTowards(transform.position, target, Time.deltaTime*speed);
+                playerRB.MovePosition(newPos);
+                yield return null;
+            }
+        }
+
+        public void Move(InputAction.CallbackContext context) {
+             float distance = Vector3.Distance(transform.position, target);
+            while (distance > float.Epsilon) {
+                Vector3 endPos = Vector3.MoveTowards(playerRB.position, target, speed * Time.deltaTime);
+                playerRB.MovePosition(endPos);
+                distance = Vector3.Distance(transform.position, target);
+
+                Vector3 direction = target - transform.position;
+                Vector3 movement = direction.normalized * speed * Time.deltaTime; 
+
+                playerRB.velocity = direction.normalized * speed;
+
+                transform.rotation = Quaternion.LookRotation(direction.normalized);
+                yield return null;
+            }
+        }
+
+        private IEnumerator PlayerMoveTowards(Vector3 target) {
+            while (Vector3.Distance(transform.position, target) > 0.1f) {
+                Vector3 destination = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+                transform.position = destination;
+                yield return null;
+            }
+        }
+
         */
-        /* void Update() {
-        if (OnClick()) {
-                Ray click = Camera.main.ScreenPointToRay(mousePositionAction);
-                RaycastHit hit;
-                if (Physics.Raycast(click, out hit)) {
-                    if (hit.collider == GetComponent<Collider>()) {
-                        moveTowards = !moveTowards;
-                    }
-                }
-            }
-        }
-
-        private void Awake() {
-            newPos = transform.position;
-            //leftClick = new InputAction(binding: "<Mouse>/leftButton");
-        }
-
-        private void Move() {
-            Vector3 startPos = transform.position;
-
-            if(Mouse.current.leftButton.wasPressedThisFrame){
-                var mousePos = pointerAction.action.ReadValue<Vector2>.();
-            }
-
-            transform.position = Vector2.Lerp (startPos, mousePos, Time.deltaTime);
-        }
-
-        private void OnEnable() {
-            leftClick.action.Enable();
-            mousePositionAction.action.Enable();
-
-            leftClick.action.performed += ctx => OnClick;
-        }
-
-        private void OnClick(InputAction.CallBackContext ctx) {
-            Move();
-        }*/
     }
 }
