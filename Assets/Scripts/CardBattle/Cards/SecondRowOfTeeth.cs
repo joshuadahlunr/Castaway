dd0105020f70207fc2a25a3a75d3383f42454843
dana <80922081+dananac@users.noreply.github.com>
2023-05-01 11:56:09 -0700

    fixed second row of teeth

--- Assets/Scripts/CardBattle/Cards/SecondRowOfTeeth.cs
+++ Assets/Scripts/CardBattle/Cards/SecondRowOfTeeth.cs
@@ -14,23 +14,32 @@ namespace CardBattle
     public class SecondRowOfTeeth : Card.ActionCardBase
     {
         // Can target player and equipment
-        public override CardFilterer.CardFilters TargetingFilters
-            => ~(CardFilterer.CardFilters.Player |
-                 CardFilterer.CardFilters.InPlay);
-        public override CardFilterer.CardFilters PlayerTargetingFilters
-            => TargetingFilters | CardFilterer.CardFilters.Player;
+        //public override CardFilterer.CardFilters TargetingFilters
+        //    => ~(CardFilterer.CardFilters.Player |
+        //         CardFilterer.CardFilters.InPlay);
+        //public override CardFilterer.CardFilters PlayerTargetingFilters
+        //    => TargetingFilters | CardFilterer.CardFilters.Player;
+
+        // Can target anything but monsters
+        public override CardFilterer.CardFilters TargetingFilters => CardFilterer.CardFilters.Monster;
+        public override CardFilterer.CardFilters MonsterTargetingFilters => CardFilterer.CardFilters.Monster;
+        public override bool CanTargetPlayer => false;
 
         /// Damages target
         public override void OnTarget(Card.CardBase _target)
         {
             var target = _target?.GetComponent<Card.HealthCardBase>();
             // Target cannot be null
-            if (NullAndPlayerCheck(target)) return;
+            if (target is null)
+            {
+                RefundAndReset();
+                return;
+            }
 
             // Increases damage by 1
             int damage = properties["primary"];
             damage += 1;
-            target.TakeDamage(damage);
+            DamageTargetOrPlayer(properties["primary"], target);
 
             SendToGraveyard();
         }
