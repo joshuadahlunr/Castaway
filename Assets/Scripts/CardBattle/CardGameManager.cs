325ffd864ab6088ddb2b23ba1d8aaacf9573e90b
Jared White (they/he) <jaredm0520@gmail.com>
2023-05-05 09:24:26 -0700

    Removed Harpoon and Cannon from Base Kit

--- Assets/Scripts/CardBattle/CardGameManager.cs
+++ Assets/Scripts/CardBattle/CardGameManager.cs
@@ -216,12 +216,12 @@ namespace CardBattle {
 					// 	level = 2,
 					// 	associatedCrewmateID = null
 					// });
-					DatabaseManager.database.Insert(new Deck.DeckListCard {
-						listID = playerDeckId,
-						name = "Damaged Harpoon",
-						level = 2,
-						associatedCrewmateID = null
-					});
+					// DatabaseManager.database.Insert(new Deck.DeckListCard {
+					// 	listID = playerDeckId,
+					// 	name = "Damaged Harpoon",
+					// 	level = 2,
+					// 	associatedCrewmateID = null
+					// });
 					DatabaseManager.database.Insert(new Deck.DeckListCard {
 						listID = playerDeckId,
 						name = "Bail Water",
@@ -234,12 +234,12 @@ namespace CardBattle {
 						level = 2,
 						associatedCrewmateID = null
 					});
-					DatabaseManager.database.Insert(new Deck.DeckListCard {
-						listID = playerDeckId,
-						name = "Cannon",
-						level = 2,
-						associatedCrewmateID = null
-					});
+					// DatabaseManager.database.Insert(new Deck.DeckListCard {
+					// 	listID = playerDeckId,
+					// 	name = "Cannon",
+					// 	level = 2,
+					// 	associatedCrewmateID = null
+					// });
 				}
 			}
 
